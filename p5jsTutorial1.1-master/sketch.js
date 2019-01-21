b994fdd78acf8b5c4dc82b65beebdc3b5ce2219f
oliverzhang1 <46880417+oliverzhang1@users.noreply.github.com>
2019-01-21 13:04:21 +0800

    Add files via upload

--- p5jsTutorial1.1-master/sketch.js
+++ p5jsTutorial1.1-master/sketch.js
@@ -8,13 +8,14 @@ function setup()
 function draw()
 {
   //create a background using RGB values
-  background(255,0,1000)
+  background(160,0,200)
 
   // change the paint brush to a specific color according to RGB values
-  fill(1000,560,255);
+  fill(0,0,205);
+
 
   // create a rectangle at x,y coordinates 10,10 with length x height at 100 x 50
   rect(100,10,350,350);
-
-
+  rect(100,10*2,350/2,350)
+  rect(100,10/2,350/2,350/5)
 }
