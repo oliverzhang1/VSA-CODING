9971ef2cae6388ebbd483c132756500e2c79608a
oliverzhang1 <46880417+oliverzhang1@users.noreply.github.com>
2019-01-21 12:28:16 +0800

    Add files via upload

--- p5jsTutorial1.1-master/sketch.js
+++ p5jsTutorial1.1-master/sketch.js
@@ -1,17 +1,20 @@
 function setup()
 {
   //create a drawing service 700px wide, 500px tall
-  createCanvas(700,500);
+  createCanvas(1000,500);
 
 }
 
 function draw()
 {
   //create a background using RGB values
-  background(255,0,0)
+  background(255,0,1000)
 
   // change the paint brush to a specific color according to RGB values
-  fill(0,0,255);
+  fill(1000,560,255);
 
   // create a rectangle at x,y coordinates 10,10 with length x height at 100 x 50
-  rect(10,10,100,50);
+  rect(100,10,350,350);
+
+
+}
