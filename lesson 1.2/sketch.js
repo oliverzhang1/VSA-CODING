c451886ec1d96e97279d56142ed4b559eb4309fa
oliverzhang1 <46880417+oliverzhang1@users.noreply.github.com>
2019-01-28 13:05:43 +0800

    Add files via upload

--- lesson 1.2/sketch.js
+++ lesson 1.2/sketch.js
@@ -6,29 +6,43 @@ function setup()
 
 function draw()
 {
-	background(255,0,0)
+	background(100,120,220)
 
 	// change the paint brush to a specific color according to RGB values
 	// this color applies only to the inside color of a shape
-	fill(0,0,255);
-
+	fill(100,70,255);
 	// change the paint brush to a specific color according to RGB values
 	// this color applies only to the outside color of a shape
-	stroke(0,0,0);
-
+	stroke(50,175,150);
 	// modifies the thickness of the border of an object
-	strokeWeight(3);
-
-
+	strokeWeight(20);
 	rect(10,10,100,50);
 	rect(10,100,100,50);
 
 
 	fill(50,255,20);
-	stroke(255,255,255);
-	strokeWeight(10);
-
+	stroke(255,20,255);
+	strokeWeight(35);
 	rect(150,10,100,50);
 	rect(150,100,100,50);
+
+  fill(20,150,20);
+	stroke(150,20,90);
+	strokeWeight(55);
+  rect(50,250,10,150);
+  rect(200,250,200,150);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
