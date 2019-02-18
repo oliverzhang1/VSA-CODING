2d762827df77e6559ab85057689da7145cd800a3
oliverzhang1 <46880417+oliverzhang1@users.noreply.github.com>
2019-02-18 13:02:18 +0800

    Add files via upload

---
+++ p5jsTutorial1.3-master/sketch.js
@@ -0,0 +1,53 @@
+function setup()
+{
+  createCanvas(700,700);
+
+}
+
+function draw()
+{
+	background(255,150,150)
+
+	fill(0,0,255);
+	stroke(0,0,0);
+	strokeWeight(3);
+
+	text("Hello World",80,80);
+
+	rect(10,10,100,50);
+
+
+	text("Oliver Zhang",20,50);
+
+	text("Donald Trump",20,130);
+	rect(10,100,100,50);
+
+	fill(255,255,255);
+
+
+
+  fill(90,223,213)
+  text("VSA STUDENT",200,220)
+
+
+
+
+
+  fill(200,50,125)
+  rect(20,200,200,100)
+
+  fill(90,223,213)
+  text("VSA teacher",200,250)
+
+  fill(10,120,255);
+  stroke(0,0,0);
+  strokeWeight(100);
+  text("CODE",300,400,)
+
+  fill(100,120,130);
+  stroke(123,234,50);
+  strokeWeight(1);
+  text("CODE",500,400,)
+
+
+}
